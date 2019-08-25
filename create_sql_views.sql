beb2c7c0280d9f7477b800ff73ae1c4f7f623051
JasonBuss <jasontbuss@gmail.com>
2019-08-25 17:15:14 -0500

    add create views script

---
+++ create_sql_views.sql
@@ -0,0 +1,20 @@
+/********************************************/
+/* Written By: Jason Buss                   */
+/* Created on: 2019-08-25                   */
+/********************************************/
+
+IF object_id(N'dbo.vw_allItems', 'V') IS NOT NULL
+	DROP VIEW dbo.vw_allItems
+GO
+
+CREATE VIEW dbo.vw_allItems AS
+SELECT
+	isnull(b.folder_name, '<<none>>') as [Folder Name]
+,	c.typeValue as [Type]
+,	A.NAME AS [Item Name]
+,	a.notes as Notes
+,	a.favorite as IsFavorite
+,	a.created as CreatedOn
+from item a
+left join folder b on a.folderid = b.folderid
+inner join item_type c on a.type = c.item_typeid
