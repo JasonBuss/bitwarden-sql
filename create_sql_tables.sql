53926955017f5eb6e9c53bec80b50ba6a0e908dd
JasonBuss <jasontbuss@gmail.com>
2019-08-25 17:15:04 -0500

    add id and timestamp fields to tables

--- create_sql_tables.sql
+++ create_sql_tables.sql
@@ -2,9 +2,14 @@
 /* Written By: Jason Buss                   */
 /* Created on: 2019-08-25                   */
 /********************************************/
-USE bwArchive
+SET NOCOUNT ON;
+USE bwArchive;
 
 /* Drop tables */
+IF OBJECT_ID('dbo.item_type', 'U') IS NOT NULL
+  DROP TABLE dbo.item_type;
+IF OBJECT_ID('dbo.field_type', 'U') IS NOT NULL
+  DROP TABLE dbo.field_type;
 IF OBJECT_ID('dbo.folder', 'U') IS NOT NULL
   DROP TABLE dbo.folder;
 IF OBJECT_ID('dbo.item', 'U') IS NOT NULL
@@ -20,10 +25,22 @@ IF OBJECT_ID('dbo.[identity]', 'U') IS NOT NULL
 IF OBJECT_ID('dbo.[card]', 'U') IS NOT NULL
   DROP TABLE dbo.[card];
 
+
 /* Create Tables */
+Create table item_type (
+	item_typeid int primary key,
+	typeValue varchar(12),
+	created datetime default getdate()
+);
+Create table field_type (
+	field_typeid int primary key,
+	typeValue varchar(12),
+	created datetime default getdate()
+);
 CREATE TABLE dbo.folder
 (	folderid UNIQUEIDENTIFIER PRIMARY KEY,
-	folder_name nvarchar(128)
+	folder_name nvarchar(128),
+	created datetime default getdate()
 );
 CREATE TABLE dbo.item
 (	itemid UNIQUEIDENTIFIER PRIMARY KEY,
@@ -33,27 +50,35 @@ CREATE TABLE dbo.item
 	name nvarchar(128),
 	notes nvarchar(max),
 	favorite nvarchar(5),
-	collectionIds uniqueidentifier
+	collectionIds uniqueidentifier,
+	created datetime default getdate()
 );
 CREATE TABLE dbo.field
-(	itemid UNIQUEIDENTIFIER,
+(	id int identity primary key,
+	itemid UNIQUEIDENTIFIER,
 	field_name nvarchar(32),
 	field_value nvarchar(32),
-	field_type int
+	field_type int,
+	created datetime default getdate()
 );
 CREATE TABLE dbo.[login]
-(	itemid UNIQUEIDENTIFIER,
+(	id int identity primary key,
+	itemid UNIQUEIDENTIFIER,
 	username nvarchar(64),
 	password nvarchar(256),
-	totp nvarchar(256)
+	totp nvarchar(256),
+	created datetime default getdate()
 );
 CREATE TABLE dbo.login_uri
-(	itemid UNIQUEIDENTIFIER,
+(	id int identity primary key,
+	itemid UNIQUEIDENTIFIER,
 	match varchar(5),
-	uri nvarchar(max)
+	uri nvarchar(max),
+	created datetime default getdate()
 );
 CREATE TABLE dbo.[identity]
-(	itemid UNIQUEIDENTIFIER,
+(	id int identity primary key,
+	itemid UNIQUEIDENTIFIER,
 	title nvarchar(32),
 	firstName nvarchar(32),
 	middleName nvarchar(32),
@@ -71,14 +96,27 @@ CREATE TABLE dbo.[identity]
 	ssn nvarchar(32),
 	username nvarchar(32),
 	passportNumber nvarchar(32),
-	licenseNumber nvarchar(32)
+	licenseNumber nvarchar(32),
+	created datetime default getdate()
 );
 CREATE TABLE dbo.[card]
-(	itemid UNIQUEIDENTIFIER,
+(	id int identity primary key,
+	itemid UNIQUEIDENTIFIER,
 	cardHolderName nvarchar(64),
 	brand nvarchar(32),
 	number nvarchar(32),
 	expMonth int,
 	expYear int,
-	code int
+	code int,
+	created datetime default getdate()
 );
+
+/* Fill Static data tables */
+INSERT INTO DBO.item_type(item_typeid, typeValue) VALUES (1, 'Login');
+INSERT INTO DBO.item_type(item_typeid, typeValue) VALUES (2, 'Secured Note');
+INSERT INTO DBO.item_type(item_typeid, typeValue) VALUES (3, 'Card');
+INSERT INTO DBO.item_type(item_typeid, typeValue) VALUES (4, 'Identity');
+
+INSERT INTO dbo.field_type(field_typeid, typeValue) VALUES (1, 'Text');
+INSERT INTO dbo.field_type(field_typeid, typeValue) VALUES (2, 'Hidden Text');
+INSERT INTO dbo.field_type(field_typeid, typeValue) VALUES (3, 'Boolean');
